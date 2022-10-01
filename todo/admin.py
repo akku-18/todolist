cf2022c1de27a235f74f3feb1775f510c492f0e2
akku-18 <akshanshgarg18@gmail.com>
2022-10-01 20:09:08 +0530

    changed something in admin.py
    unregister and register user

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Todo
+from django.contrib.auth.models import User
 
 # Register your models here.
-
+admin.site.unregister(User)
 admin.site.register(Todo)
+admin.site.register(User)
